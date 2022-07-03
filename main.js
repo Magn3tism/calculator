b00d35de4f79f14a99cdb999709ea4967d543c62
magn3tism <chapagainsakshyam@gmail.com>
2022-07-03 20:18:53 +0545

    Add functionality to remaining buttons

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const numberButtons = Array.from(document.getElementsByClassName("number"));
 const operatorButtons = Array.from(document.getElementsByClassName("operator"));
 const result = document.getElementById("result");
+
 const current = document.getElementById("current");
 const previous = document.getElementById("previous");
 
+const clear = document.getElementById("clear");
+const backsapce = document.getElementById("backspace");
+const percentage = document.getElementById("percentage");
+
 let string = "";
 let numString = "";
+let pressed = 0;
 
 function add(a, b) {
   return a + b;
@@ -20,6 +26,12 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+  if (!b) {
+    string = "";
+    numString = "";
+    previous.textContent = "0";
+    return "Infinity";
+  }
   return a / b;
 }
 
@@ -45,6 +57,8 @@ numberButtons.forEach((numberButton) => {
     string += e.target.dataset.value;
     numString += e.target.dataset.value;
     current.textContent = numString;
+
+    pressed = 1;
   });
 });
 
@@ -57,11 +71,12 @@ operatorButtons.forEach((operatorButton) => {
       string.includes("÷")
     ) {
       string = String(operate(string));
-      console.log(`O: ${string}`);
     }
     string += e.target.dataset.value;
     previous.textContent = string;
     numString = "";
+
+    pressed = 2;
   });
 });
 
@@ -69,6 +84,35 @@ result.addEventListener("click", () => {
   previous.textContent = string;
   string = operate(string);
   current.textContent = string;
+  string = "";
+  numString = "";
+});
+
+clear.addEventListener("click", () => {
+  string = "";
+  numString = "";
+  current.textContent = "0";
+  previous.textContent = "0";
+});
+
+backsapce.addEventListener("click", () => {
+  string = string.substring(0, string.length - 1);
+
+  if (pressed === 1) {
+    numString = numString.substring(0, numString.length - 1);
+    if (numString) {
+      current.textContent = numString;
+    } else {
+      current.textContent = 0;
+    }
+  } else if (pressed === 2) {
+    previous.textContent = string;
+  }
+});
+
+percentage.addEventListener("click", () => {
+  previous.textContent = "9";
+  current.textContent = `${string}%`;
   string = "";
   numString = "";
 });
